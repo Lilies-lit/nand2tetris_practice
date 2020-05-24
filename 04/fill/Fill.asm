0a4c58dfe3d59b14e91868ad871512248f7abae6
lilies_lit <naruylucky@gmail.com>
2020-05-24 14:24:14 +0900

    Add fill screen asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,70 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+@i
+M = 0
+(LOOPA)
+@i
+D = M
+@SCREEN
+A = A + D
+M = 0
+
+@i
+M = M + 1
+
+@i
+D = M
+@8192
+D = D - A
+
+@INITA
+D;JGE
+
+@KBD
+D = M;
+@INITB
+D;JGT
+
+@LOOPA
+0;JMP
+
+(INITA)
+@i
+M = 0
+@LOOPA
+0;JMP
+
+
+
+(LOOPB)
+@i
+D = M
+@SCREEN
+A = A + D
+M = 1
+
+@i
+M = M + 1
+
+@i
+D = M
+@8192
+D = D - A
+
+@INITB
+D;JGE
+
+@KBD
+D = M;
+@INITA
+D;JEQ
+@LOOPB
+0;JMP
+
+(INITB)
+@i
+M = 0
+@LOOPB
+0;JMP
+
